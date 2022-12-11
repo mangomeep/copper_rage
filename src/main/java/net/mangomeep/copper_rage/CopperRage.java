427c07dd95f89ff3fba556ba7debeff8a48f080a
mangomeep <nickdasdf@gmail.com>
2022-12-11 23:04:49 +1300

    first build

---
+++ src/main/java/net/mangomeep/copper_rage/CopperRage.java
@@ -0,0 +1,14 @@
+package net.mangomeep.copper_rage;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CopperRage implements ModInitializer {
+	public static final String MOD_ID = "copper_rage";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/mangomeep/copper_rage/CopperRageClient.java
@@ -0,0 +1,10 @@
+package net.mangomeep.copper_rage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperRageClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient () {
+
+    }
+}
